665be474401f6b10078fdc2efd92eed0bf457afc
Joaquin Jimenez Garcia <garcia.jjimenez@gmail.com>
2017-01-12 17:17:41 +0100

    Initial commit

---
+++ src/com/joaquinjimenezgarcia/Main.java
@@ -0,0 +1,21 @@
+package com.joaquinjimenezgarcia;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
+
+/*
+Pregunta por cartones
+Bolas del 1 al 90
+Cartones con 3 filas de 5 números (15 nums)
+
+Generar bolas (descartando las que sales)
+Los cartones presentan los números ordenados de menor a mayor
+
+Hay que mostrar qué cartón ha ganado con los números que han salido
+Y el total de bolas que han salido con sus números
+
+ */
